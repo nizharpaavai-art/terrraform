3b0c082d4c9a0ab11864a6a1cfad5992f32a245e
nizhal <nizharpaavai@gmail.com>
2025-12-19 03:47:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-0abcdef1234567890"  # Replace with your AMI
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH"
+  default     = "serverkey"   # Replace with your key
+}
 
